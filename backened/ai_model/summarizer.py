2748bd09ec0cc191229257c90c164508e3ce264e
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-28 22:25:34 +1000

    Update summarizer.py

--- backened/ai_model/summarizer.py
+++ backened/ai_model/summarizer.py
@@ -1,9 +1,9 @@
 ### research_summarizer_app/backend/ai_model/summarizer.py
 
+import streamlit as st
 from transformers import pipeline
-import torch
 
-summarizer = pipeline("summarization")
+@st.cache_resource(show_spinner=False)
+def load_summarizer():
+    return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
-def generate_summary(text):
-    return summarizer(text[:1000])[0]['summary_text']

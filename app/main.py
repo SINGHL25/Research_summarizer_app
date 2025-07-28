4b611df5d6d250c59fd4a37b6aa45f98a8e4b3b8
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-28 22:18:03 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,17 @@ import sys
 import os
 import torch
 
+
+import streamlit as st
+from transformers import pipeline
+
+@st.cache_resource(show_spinner=False)
+def load_summarizer():
+    return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+
+summarizer = load_summarizer()
+
+
 # Add the parent directory (project root) to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 

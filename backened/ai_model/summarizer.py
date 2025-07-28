c8d9d7d199e8506f0a242aabea376fcfe846279d
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-28 22:10:55 +1000

    Update summarizer.py

--- backened/ai_model/summarizer.py
+++ backened/ai_model/summarizer.py
@@ -1,6 +1,8 @@
 ### research_summarizer_app/backend/ai_model/summarizer.py
 
 from transformers import pipeline
+import torch
+
 summarizer = pipeline("summarization")
 
 def generate_summary(text):
